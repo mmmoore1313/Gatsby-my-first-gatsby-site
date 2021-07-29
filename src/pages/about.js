e5332a2e4d29d668e68ce3fa4cceb2764f80c86d
Matt Moore <Matt.M.Moore13@outlook.com>
2021-07-29 03:54:56 -0400

    Added some styling to the site (Part 2 of the Gatsby Tutorial)

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return(
+    <Layout pageTitle='About Page'>
+      <p>I'm the creater of this site, which was built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
