388f32e4d77cf2bd0535fa197ba43e36a4911bd4
Matt Moore <Matt.M.Moore13@outlook.com>
2021-07-29 04:29:25 -0400

    - Added the gatsby-plugin-image, gatsby-plugin-sharp plugins to the gatsby-config.js - Added two .jpg images to the src file - Added an image to the Homepage using the StaticImage tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Two dogs sharing a bed with a view"
+        src="../images/dogsonabed.jpg"
+      />
     </Layout>
   )
 }
